aa348735c0743212c2130cf23a8b039a36e65eca
Vinhday <vinhoangduong@gmail.com>
2022-10-18 18:32:16 +0700

    ex9

---
+++ EX9/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<int> arr)
+    {
+        int pos = 0;
+        int neg = 0;
+        int zero = 0;
+
+        for (int i = 0; i < arr.Count; i++)
+        {
+            if (arr[i] < 0)
+            {
+                neg++;
+            }
+            if (arr[i] > 0)
+            {
+                pos++;
+            }
+            if (arr[i] == 0)
+            {
+                zero++;
+            }
+
+        }
+        Console.WriteLine($"{(float)pos / arr.Count:F6}");
+        Console.WriteLine($"{(float)neg / arr.Count:F6}");
+        Console.WriteLine($"{(float)zero / arr.Count:F6}");
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.plusMinus(arr);
+    }
+}
