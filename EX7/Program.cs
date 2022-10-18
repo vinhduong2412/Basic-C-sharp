b0d59b2f41764800cd143ff72f5efb6a23a500f8
Vinhday <vinhoangduong@gmail.com>
2022-10-18 18:10:33 +0700

    ex7

---
+++ EX7/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'surfaceArea' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY A as parameter.
+     */
+
+    public static int surfaceArea(List<List<int>> A)
+    {
+        int area = 0;
+        // Complete this function
+        for (int i = 0; i < A.Count; i++)
+        {
+            for (int j = 0; j < A[i].Count; j++)
+            {
+                area += A[i][j] * 4 + 2;
+                if (i > 0)
+                {
+                    area -= Math.Min(A[i - 1][j], A[i][j]) * 2;
+                }
+                if (j > 0)
+                {
+                    area -= Math.Min(A[i][j - 1], A[i][j]) * 2;
+                }
+            }
+        }
+        return area;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int H = Convert.ToInt32(firstMultipleInput[0]);
+
+        int W = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<List<int>> A = new List<List<int>>();
+
+        for (int i = 0; i < H; i++)
+        {
+            A.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(ATemp => Convert.ToInt32(ATemp)).ToList());
+        }
+
+        int result = Result.surfaceArea(A);
+
+        Console.WriteLine(result);
+    }
+}
